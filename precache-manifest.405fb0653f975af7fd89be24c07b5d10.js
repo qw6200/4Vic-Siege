634813e105a9ff4968b464dbd0885da2ae523134
jalee <jalee@constantcontact.com>
2020-01-14 17:07:54 -0500

    Updates

---
+++ precache-manifest.405fb0653f975af7fd89be24c07b5d10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b05e5c705fc534456f930dc6bf8fbc",
+    "url": "/4Vic-Siege/index.html"
+  },
+  {
+    "revision": "e92a4b5a2df28a13543d",
+    "url": "/4Vic-Siege/static/css/main.447a6e18.chunk.css"
+  },
+  {
+    "revision": "ab4a57b59e24e384c290",
+    "url": "/4Vic-Siege/static/js/2.324ca0ad.chunk.js"
+  },
+  {
+    "revision": "bd58a53ea30c71d23a62143160dc052c",
+    "url": "/4Vic-Siege/static/js/2.324ca0ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e92a4b5a2df28a13543d",
+    "url": "/4Vic-Siege/static/js/main.e153d43e.chunk.js"
+  },
+  {
+    "revision": "5d792f71e965754aa277",
+    "url": "/4Vic-Siege/static/js/runtime-main.7c0a4851.js"
+  }
+]);
